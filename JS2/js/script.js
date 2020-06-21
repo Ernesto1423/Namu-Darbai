a67a4f824e2f7e0073bf15fce3d085b8f2bf411e
Ernesto1423 <66556723+Ernesto1423@users.noreply.github.com>
2020-06-21 16:02:28 +0300

    idedu pirmus JS darbus

---
+++ JS2/js/script.js
@@ -0,0 +1,12 @@
+// 1 block. declarations.
+var colorTitle = "red";
+var colorParagraph = "blue";
+var title = "Skyrius1";
+var article = "Magna ita offendit, anim quibusdam philosophari, an quid praetermissum nam nisi despicationes nescius elit commodo. Ita et distinguatur de id pariatur id laborum, ubi enim probant proident ad duis aut pariatur, sunt uidicem consursionibus nam fore ab voluptate do nulla consectetur voluptate elit aliquip, legam ad admondum et quid. Quae probant an commodo, litteris de summis ullamco. Est legam nostrud arbitrantur ex si a culpa fore quorum est an amet quis est aliquip, enim incididunt quo praetermissum. Eu pariartur ab fabulas. Minim nescius cohaerescant, qui quorum eram de singulis.";
+var textSymbols = "Tekste yra 5958 simbolių";
+
+// 2 actions
+output = '<h1 style="color: ' + colorTitle + ';">' + title + '</h1>' + '<p style ="color: ' + colorParagraph + ';">' + article + '</p>' + '<p>' + textSymbols + '</p>';
+
+// 3 output
+document.write(output);
